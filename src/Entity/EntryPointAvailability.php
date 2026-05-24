55f79324c80bd2e412ebec1e189e29dc31319108
pheldu95 <phelps.duncan@gmail.com>
2026-05-24 14:04:43 -0500

    added unique constraint to EntryPointAvailability so that there can only be one per date

--- src/Entity/EntryPointAvailability.php
+++ src/Entity/EntryPointAvailability.php
@@ -7,6 +7,7 @@ use App\Repository\EntryPointAvailabilityRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: EntryPointAvailabilityRepository::class)]
+#[ORM\UniqueConstraint(fields: ['monitoredDate', 'entryPoint'])]
 #[ApiResource]
 class EntryPointAvailability
 {
